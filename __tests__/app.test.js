f0c764d8c39cb83f97baa4b957dd9dad1c08e3e9
Taylor Reichner <75050771+taylorreichner@users.noreply.github.com>
2021-06-09 15:49:38 -0700

    Data munge 6 companys and added flush feature to renew db (#1)
    * added first fetch and munge function
    
    * added 6 companys and flush feature to render new jobs

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,15 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const fetchLyft = require('../lib/utils/seed-data');
+const fetch  = require('node-fetch');
 
 describe('jobs-be-serer routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+
+ 
+  
 });
---
+++ sql/setup.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS all_jobs;
+
+CREATE TABLE all_jobs (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT,
+    company TEXT,
+    url TEXT,
+    post_date DATE,
+    location TEXT 
+)
