8b8232bbc2a24b03c2bab6a2798fdb9b6011f9f7
Colin Bassett <65682501+ccbass@users.noreply.github.com>
2021-08-13 15:55:30 -0700

    Fix/cors cookies (#13)
    * modified cors to ENV and cookie attr.
    
    * fixed cors, cookies to secure;  Added savejobs route

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS all_jobs;
+-- DROP TABLE IF EXISTS all_jobs;
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS notes CASCADE;
 DROP TABLE IF EXISTS events CASCADE;
@@ -11,14 +11,14 @@ DROP SEQUENCE IF EXISTS columns_column_id_seq CASCADE;
 DROP SEQUENCE IF EXISTS job_apps_app_id_seq CASCADE;
 
 
-CREATE TABLE all_jobs (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    title TEXT,
-    company TEXT,
-    url TEXT,
-    post_date DATE,
-    location TEXT 
-);
+-- CREATE TABLE all_jobs (
+--     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+--     title TEXT,
+--     company TEXT,
+--     url TEXT,
+--     post_date DATE,
+--     location TEXT 
+-- );
 
 
 
@@ -48,6 +48,7 @@ CREATE SEQUENCE columns_column_id_seq
 CREATE TABLE columns (
   column_id BIGINT PRIMARY KEY NOT NULL DEFAULT nextval('columns_column_id_seq'),
   name TEXT,
+  is_archive BOOL DEFAULT FALSE,
   col_position INT,
   job_pos INT ARRAY,
   linked_user_id INT,
@@ -118,27 +119,29 @@ CREATE TABLE contacts (
         REFERENCES job_apps(app_id)
         ON DELETE SET NULL
 );
+
 -- INSERTING TEST DATA BELOW
--- user1 - abc123
 
 -- INSERT INTO users (username, email, password) 
 -- 	VALUES 
---     	('user1', 'one@one.com', '$2b$12$E/.Xntp5e082S0uxhTx6N.ldDqdPkgNDVO4eRKOvEAnoPKCWPncYu');
+--     	('jobseeker', 'one@one.com', '$2b$12$E/.Xntp5e082S0uxhTx6N.ldDqdPkgNDVO4eRKOvEAnoPKCWPncYu');
+
+-- INSERT INTO columns (name, col_position, is_archive, job_pos, linked_user_id) 
+-- 	VALUES 
+--       ('Storage', '0', 'true', '{}', '100');
 
 -- INSERT INTO columns (name, col_position, job_pos, linked_user_id) 
 -- 	VALUES 
---       ('progress', '0', '{700, 701, 702}', '100'),
---     	('todo', '1', '{703, 704}', '100'),
---       ('offer', '2', '{705}', '100');
-
+--       ('To Do', '1', '{700, 701, 702}', '100'),
+--     	('In Progress', '2', '{703, 704}', '100'),
+--       ('Waiting', '3', '{705}', '100');
 
 -- INSERT INTO job_apps (position, company, job_url, linked_user_id)
 --   VALUES
---       ('SWE A', 'Amazon', 'http://a', '100');
---       ('SWE B', 'Google', 'http://b', '100'),
---       ('SWE C', 'Reddit', 'http://c', '100'),
---       ('SWE D', 'Twitter', 'http://d', '100'),
---       ('SWE E', 'FB', 'http://e', '100'),
---       ('SWE F', 'Insta', 'http://f', '100');
+--       ('Software Engineer, AppSec', 'Stripe', 'https://stripe.com/jobs?gh_jid=3117424', '100'),
+--       ('Software Engineer, Full-stack', 'Strava', 'https://boards.greenhouse.io/strava/jobs/2924484', '100'),
+--       ('Ops Engineer', 'Lyft', 'https://boards.greenhouse.io/lyft/jobs/5277277002?gh_jid=5277277002', '100'),
+--       ('Software Engineer, ML', 'Coinbase', 'https://www.coinbase.com/careers/2416374?gh_jid=2416374', '100'),
+--       ('Software Engineer, Server', 'Strava', 'https://boards.greenhouse.io/strava/jobs/2927224', '100'),
+--       ('Software Engineer, App Core', 'GitHub', 'https://boards.greenhouse.io/github/jobs/3005977', '100');
 
--- UPDATE columns SET job_pos='{708}' WHERE column_id=504;
